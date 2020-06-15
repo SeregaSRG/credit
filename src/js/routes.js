ea7819775d6f6c0a0d249fbb4f6aee23b320bd4a
Serega <monohrom98@gmail.com>
2020-06-15 04:09:20 +0300

    init project

---
+++ src/js/routes.js
@@ -0,0 +1,15 @@
+import init from '../pages/init.vue'
+import login from '../pages/login.vue'
+
+let routes = [
+  {
+    path: '/',
+    component: init,
+  },
+  {
+    path: '/login',
+    component: login,
+  },
+]
+
+export default routes
